76137cd69afe93d65d4f3f6e67be43503650df6a
Linyiyu <theo@opstw.info>
2026-08-24 20:03:01 +0800

    Localize notification experiment settings

--- AutoRetainer/UI/NeoUI/Experiments/Notifications.cs
+++ AutoRetainer/UI/NeoUI/Experiments/Notifications.cs
@@ -3,14 +3,14 @@ public class Notifications : ExperimentUIEntry
 {
     public override void Draw()
     {
-        ImGui.Checkbox($"Display overlay notification if one of retainers has completed a venture", ref C.NotifyEnableOverlay);
-        ImGui.Checkbox($"Do not display overlay in duty or combat", ref C.NotifyCombatDutyNoDisplay);
-        ImGui.Checkbox($"Include other characters", ref C.NotifyIncludeAllChara);
-        ImGui.Checkbox($"Ignore other characters that have not been enabled in MultiMode", ref C.NotifyIgnoreNoMultiMode);
-        ImGui.Checkbox($"Display notification in game chat", ref C.NotifyDisplayInChatX);
-        ImGuiEx.Text($"If game is inactive: (requires NotificationMaster to be installed and enabled)");
-        ImGui.Checkbox($"Send desktop notification on retainers available", ref C.NotifyDeskopToast);
-        ImGui.Checkbox($"Flash taskbar", ref C.NotifyFlashTaskbar);
-        ImGui.Checkbox($"Do not notify if AutoRetainer is enabled or MultiMode is running", ref C.NotifyNoToastWhenRunning);
+        ImGui.Checkbox($"若任一僱員完成探索，顯示浮層通知", ref C.NotifyEnableOverlay);
+        ImGui.Checkbox($"副本或戰鬥中不要顯示浮層", ref C.NotifyCombatDutyNoDisplay);
+        ImGui.Checkbox($"包含其他角色", ref C.NotifyIncludeAllChara);
+        ImGui.Checkbox($"忽略未啟用 MultiMode 的其他角色", ref C.NotifyIgnoreNoMultiMode);
+        ImGui.Checkbox($"在遊戲聊天顯示通知", ref C.NotifyDisplayInChatX);
+        ImGuiEx.Text($"遊戲不在前景時：（需要安裝並啟用 NotificationMaster）");
+        ImGui.Checkbox($"僱員可處理時送出桌面通知", ref C.NotifyDeskopToast);
+        ImGui.Checkbox($"閃爍工作列", ref C.NotifyFlashTaskbar);
+        ImGui.Checkbox($"AutoRetainer 已啟用或 MultiMode 運作中時不要通知", ref C.NotifyNoToastWhenRunning);
     }
 }
